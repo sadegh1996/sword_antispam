42bd0ddeb298b22fffc0f802208bde60ba9df8e8
sadegh1996 <sa.koorosh@gmail.com>
2016-04-28 18:01:55 +0430

    Update info.lua

--- plugins/info.lua
+++ plugins/info.lua
@@ -1,6 +1,6 @@
 
 do
-local ali_ghoghnoos = 100577715 --put your id here(BOT OWNER ID)
+local sadegha_1996 = 77076695 --put your id here(BOT OWNER ID)
 
 local function setrank(msg, name, value) -- setrank function
   local hash = nil
